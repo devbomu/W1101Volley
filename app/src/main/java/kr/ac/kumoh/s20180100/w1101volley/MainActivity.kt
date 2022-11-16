70775bf2a3637d75ffdc1a04432532cdc596db56
BomuKim <styg0604@gmail.com>
2022-11-16 16:44:17 +0900

    JSONArray 출력

--- app/src/main/java/kr/ac/kumoh/s20180100/w1101volley/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20180100/w1101volley/MainActivity.kt
@@ -2,16 +2,19 @@ package kr.ac.kumoh.s20180100.w1101volley
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import com.android.volley.Request
 import com.android.volley.RequestQueue
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
 import kr.ac.kumoh.s20180100.w1101volley.databinding.ActivityMainBinding
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var queue: RequestQueue
+    private val movies = ArrayList<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,7 +29,19 @@ class MainActivity : AppCompatActivity() {
                 url,
                 null,
                 {
-                    Toast.makeText(application, it.toString(), Toast.LENGTH_LONG).show()
+                    //Toast.makeText(application, it.toString(), Toast.LENGTH_LONG).show()
+                    movies.clear()
+                    parseJson(it)
+//                    Toast.makeText(application,
+//                        movies.toString()
+//                            .replace(",", "\n")
+//                            .replace("[\\[\\]]".toRegex(), ""),
+//                        Toast.LENGTH_LONG).show()
+                    binding.listMovies.adapter = ArrayAdapter<String>(
+                        this,
+                        android.R.layout.simple_list_item_1,
+                        movies
+                    )
                 },
                 {
                     Toast.makeText(application, it.toString(), Toast.LENGTH_LONG).show()
@@ -34,6 +49,15 @@ class MainActivity : AppCompatActivity() {
             )
 
             queue.add(request)
+        }
+    }
+
+    private fun parseJson(obj: JSONObject){
+        val items = obj.getJSONObject("data").getJSONArray("movies")
+        for(i in 0 until items.length()){
+            val item: JSONObject = items[i] as JSONObject
+            val title = item.getString("title_long")
+            movies.add(title)
         }
     }
 }
